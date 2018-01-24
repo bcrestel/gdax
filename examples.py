ccb3192eabc747a4e80c135f8441107714369c46
Benjamin Crestel [lenovox220] <crestel@ices.utexas.edu>
2018-01-23 22:08:45 -0600

    simplify

--- examples.py
+++ examples.py
@@ -2,21 +2,10 @@ from datetime import datetime
 
 from gdax import GDAX
 
-def btc_usd_1min(start, end):
-  return GDAX('BTC-USD').fetch(start, end, 1)
-
-def btc_usd_30min(start, end):
-  return GDAX('BTC-USD').fetch(start, end, 30)
-
-
-def btc_usd_1day(start, end):
-  return GDAX('BTC-USD').fetch(start, end, 1440)
-
 
 if __name__ == "__main__":
-  #data_frame = btc_usd_1day(datetime(2018, 1, 1), datetime(2018, 1, 30))
-  data_frame = btc_usd_1min(datetime(2018, 1, 21), datetime(2018, 1, 22))
-  print(data_frame)
-
-  # Save to CSV.
-  data_frame.to_csv('data.csv')
+    # actual time is 6h prior to the one entered
+    df = GDAX('BTC-USD').fetch(\
+    datetime(2018, 1, 2, 14, 0, 0), datetime(2018, 1, 2, 14, 30, 0), 1)
+    print(df)
+    df.to_csv('data.csv')
